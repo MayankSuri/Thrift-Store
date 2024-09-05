800231a7eccf99d3978986ead0acaeccf16b230d
MayankSuri <mayanksuri651@gmail.com>
2024-09-05 13:48:21 +0530

    Add files via upload

---
+++ final_project/includes/common.php
@@ -0,0 +1,9 @@
+<?php
+$con=mysqli_connect("localhost","root","","ecommerce");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+else
+{
+    echo "Connection Succesfull";
+}
---
+++ final_project/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;The Indian Thrift Store | All Rights Reserved | Contact Us: +91 6239041409 | 7082089338</b></span></div>
+</footer>
+    
